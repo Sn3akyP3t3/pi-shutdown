ca31581c96d04413eb9f3823e3149dd90b9a1835
Sn3akyP3t3 <1790256+Sn3akyP3t3@users.noreply.github.com>
2022-06-18 13:10:12 -0500

    Refactoring
    Refactored With Use Of Class and Applied Many PEP Recommendations

--- pishutdown.py
+++ pishutdown.py
@@ -1,50 +1,48 @@
 #!/usr/bin/python
 # shutdown/reboot(/power on) Raspberry Pi with pushbutton
-
-import RPi.GPIO as GPIO
-from subprocess import call
-from datetime import datetime
 import time
-
-# pushbutton connected to this GPIO pin, using pin 5 also has the benefit of
-# waking / powering up Raspberry Pi when button is pressed
-shutdownPin = 5
-
-# if button pressed for at least this long then shut down. if less then reboot.
-shutdownMinSeconds = 3
-
-# button debounce time in seconds
-debounceSeconds = 0.01
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(shutdownPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-buttonPressedTime = None
+from datetime import datetime
+from subprocess import call
+import RPi.GPIO as GPIO
 
 
-def buttonStateChanged(pin):
-    global buttonPressedTime
+class ShutdownManager(object):
+	def __init__(self):
+		# pushbutton connected to this GPIO pin, using pin 5 also has the benefit of
+		# waking / powering up Raspberry Pi when button is pressed
+		self.SHUTDOWN_PIN = 5
 
-    if not (GPIO.input(pin)):
-        # button is down
-        if buttonPressedTime is None:
-            buttonPressedTime = datetime.now()
-    else:
-        # button is up
-        if buttonPressedTime is not None:
-            elapsed = (datetime.now() - buttonPressedTime).total_seconds()
-            buttonPressedTime = None
-            if elapsed >= shutdownMinSeconds:
-                # button pressed for more than specified time, shutdown
-                call(['shutdown', '-h', 'now'], shell=False)
-            elif elapsed >= debounceSeconds:
-                # button pressed for a shorter time, reboot
-                call(['shutdown', '-r', 'now'], shell=False)
+		# if button pressed for at least this long then shut down. if less then reboot.
+		self.SHUTDOWN_MIN_SECONDS = 3
+
+		# button debounce time in seconds
+		self.DEBOUNCE_SECONDS = 0.01
+
+		GPIO.setmode(GPIO.BOARD)
+		GPIO.setup(self.SHUTDOWN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+		self.BUTTON_PRESSED_TIME = None
+
+	def ButtonStateChanged(self, pin):
+		if not GPIO.input(pin) and self.BUTTON_PRESSED_TIME is None:
+			# button is down
+			self.BUTTON_PRESSED_TIME = datetime.now()
+		elif self.BUTTON_PRESSED_TIME is not None:
+			# button is up
+			elapsed = (datetime.now() - self.BUTTON_PRESSED_TIME).total_seconds()
+			self.BUTTON_PRESSED_TIME = None
+			if elapsed >= self.SHUTDOWN_MIN_SECONDS:
+				# button pressed for more than specified time, shutdown
+				call(['shutdown', '-h', 'now'], shell=False)
+			elif elapsed >= self.DEBOUNCE_SECONDS:
+				# button pressed for a shorter time, reboot
+				call(['shutdown', '-r', 'now'], shell=False)
 
 
-# subscribe to button presses
-GPIO.add_event_detect(shutdownPin, GPIO.BOTH, callback=buttonStateChanged)
+if __name__ == "__main__":
+	sdm = ShutdownManager()
+	# subscribe to button presses
+	GPIO.add_event_detect(sdm.SHUTDOWN_PIN, GPIO.BOTH, callback=sdm.ButtonStateChanged)
 
-while True:
-    # sleep to reduce unnecessary CPU usage
-    time.sleep(5)
+	while True:
+		# sleep to reduce unnecessary CPU usage
+		time.sleep(5)
